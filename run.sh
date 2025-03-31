a0c18b7632bcd2ffb3e2ec5e7352bb7c94bcaddb
Kevin Leo <lgxz@duck.com>
2025-03-31 16:51:55 +0200

    Add Dockerfile.deb for Debian based image

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+IMAGE="lgxz/aipython:latest"
 DOCKER="docker run -v $(pwd)/aipython.toml:/app/aipython.toml -v $(pwd)/work:/app/work"
 
 if [ "$1" = "--ttyd" ]; then
-    ${DOCKER} -d --name aipython-ttyd -p 8080:80 lgxz/aipython:latest --ttyd
+    ${DOCKER} -d --name aipython-ttyd -p 8080:80 $IMAGE --ttyd
 else
-    ${DOCKER} -it --rm --name aipython lgxz/aipython:latest
+    ${DOCKER} -it --rm --name aipython $IMAGE
 fi
