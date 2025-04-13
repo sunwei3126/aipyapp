8e5ddebdbaae2c9df13a8df7b2932ae064bf9ddf
notsobad <wangxiaohugg@gmail.com>
2025-04-13 20:14:29 +0800

    fix gui problem

--- aipyapp/__main__.py
+++ aipyapp/__main__.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
+from pathlib import Path
+
+from .aipy.config import SETTINGS_FILES
 
 def main():
+    settings_files_help = "\n".join(map(str, SETTINGS_FILES))
+
+    config_help_message = (
+        f"Toml config file. If not provided, searches in:\n{settings_files_help}"
+    )
+
     def parse_args():
         import argparse
-        parser = argparse.ArgumentParser(description="Python use - AIPython")
-        parser.add_argument("-c", '--config', type=str, default=None, help="Toml config file")
+        parser = argparse.ArgumentParser(description="Python use - AIPython", formatter_class=argparse.RawTextHelpFormatter)
+        parser.add_argument("-c", '--config', type=str, default="aipy.toml",
+                            help=config_help_message) # Use the generated help message
         parser.add_argument('-p', '--python', default=False, action='store_true', help="Python mode")
         parser.add_argument('-g', '--gui', default=False, action='store_true', help="GUI mode")
+
         parser.add_argument('cmd', nargs='?', default=None, help="Task to execute, e.g. 'Who are you?'")
         return parser.parse_args()
-    
+
     args = parse_args()
+
     if args.python:
         from .main import main as aipy_main
     elif args.gui:
