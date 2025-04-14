271a0e723f72824e5202c89903767116fa50fc5b
Kevin Leo <lgxz@duck.com>
2025-04-14 12:53:36 +0200

    - Only build images for tag - mkdir -p work

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 IMAGE="aipyapp/aipy:latest"
 DOCKER="docker run -v $(pwd)/aipy.toml:/app/aipy.toml -v $(pwd)/work:/app/work"
 
+mkdir -p work
+
 if [ "$1" = "--ttyd" ]; then
     ${DOCKER} -d --name aipy-ttyd -p 8080:80 $IMAGE --ttyd
 else
