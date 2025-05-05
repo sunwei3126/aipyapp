8b61ee2b754e5ffda3226505a75afc2533640e13
Kevin Leo <lgxz@duck.com>
2025-05-05 14:19:41 +0200

    插件接口变化

--- aipyapp/plugins/aio_api.py
+++ aipyapp/plugins/aio_api.py
@@ -1,9 +1,9 @@
 class Plugin:
     """Trustoken all-in-one API plugin for AIPY
     """
-    def on_exec(self, blocks):
+    def on_exec(self, block):
         """
         执行代码事件
-        param: blocks
+        param: block
         """
-        blocks['main'] = blocks['main'].replace('https://restapi.amap.com/', 'https://api.trustoken.cn/aio-api/amap/')
+        block['content'] = blocks['content'].replace('https://restapi.amap.com/', 'https://api.trustoken.cn/aio-api/amap/')
